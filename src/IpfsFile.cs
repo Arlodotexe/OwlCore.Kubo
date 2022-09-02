1fe5c71defa4373ce2bb0e201d51ab917cbd908c
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-09-02 13:47:55 -0500

    Added functional IpfsFile and IpfsFolder

---
+++ src/IpfsFile.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Diagnostics;
+using Ipfs;
+using Ipfs.Http;
+using OwlCore.Storage;
+using System.Runtime.CompilerServices;
+
+namespace OwlCore.Kubo
+{
+    /// <summary>
+    /// A file that resides on IPFS.
+    /// </summary>
+    public class IpfsFile : IFile
+    {
+        private readonly IpfsClient _client;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="IpfsFile"/>.
+        /// </summary>
+        /// <param name="cid">The CID of the file, such as "QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V".</param>
+        /// <param name="client">The IPFS Client to use for retrieving the content.</param>
+        public IpfsFile(Cid cid, IpfsClient client)
+        {
+            Name = cid;
+            Id = cid;
+            _client = client;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="IpfsFile"/>.
+        /// </summary>
+        /// <param name="cid">The CID of the file, such as "QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V".</param>
+        /// <param name="name">The name of the file.</param>
+        /// <param name="client">The IPFS Client to use for retrieving the content.</param>
+        public IpfsFile(Cid cid, string name, IpfsClient client)
+        {
+            Name = !string.IsNullOrWhiteSpace(name) ? name : cid;
+            Id = cid;
+            _client = client;
+        }
+
+        /// <inheritdoc/>
+        public string Id { get; }
+
+        /// <inheritdoc/>
+        public string Name { get; }
+
+        /// <inheritdoc/>
+        public Task<Stream> OpenStreamAsync(FileAccess accessMode = FileAccess.Read, CancellationToken cancellationToken = default)
+        {
+            if (accessMode.HasFlag(FileAccess.Write))
+                throw new NotSupportedException("Attempted to write data to an immutable file on IPFS.");
+
+            return _client.FileSystem.ReadFileAsync(Id);
+        }
+    }
+}
---
+++ src/IpfsFolder.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Diagnostics;
+using Ipfs;
+using Ipfs.Http;
+using OwlCore.Storage;
+using System.Runtime.CompilerServices;
+
+namespace OwlCore.Kubo
+{
+    /// <summary>
+    /// A folder that resides on IPFS.
+    /// </summary>
+    public class IpfsFolder : IFolder
+    {
+        private readonly IpfsClient _client;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="IpfsFolder"/>.
+        /// </summary>
+        /// <param name="cid">The CID of the folder, such as "QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V".</param>
+        /// <param name="client">The IPFS Client to use for retrieving the content.</param>
+        public IpfsFolder(Cid cid, IpfsClient client)
+        {
+            Id = cid;
+            Name = cid;
+            _client = client;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="IpfsFolder"/>.
+        /// </summary>
+        /// <param name="cid">The CID of the folder, such as "QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V".</param>
+        /// <param name="name">The name of the folder.</param>
+        /// <param name="client">The IPFS Client to use for retrieving the content.</param>
+        public IpfsFolder(Cid cid, string name, IpfsClient client)
+        {
+            Id = cid;
+            Name = !string.IsNullOrWhiteSpace(name) ? name : cid;
+            _client = client;
+        }
+
+        /// <inheritdoc/>
+        public string Id { get; }
+
+        /// <inheritdoc/>
+        public string Name { get; }
+
+        /// <inheritdoc/>
+        public async IAsyncEnumerable<IStorable> GetItemsAsync(StorableType type = StorableType.All, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            var itemInfo = await _client.FileSystem.ListFileAsync(Id, cancellationToken);
+            Guard.IsTrue(itemInfo.IsDirectory);
+
+            foreach (var link in itemInfo.Links)
+            {
+                Guard.IsNotNullOrWhiteSpace(link.Id);
+                var linkedItemInfo = await _client.FileSystem.ListFileAsync(link.Id, cancellationToken);
+
+                if (linkedItemInfo.IsDirectory)
+                    yield return new IpfsFolder(link.Id, link.Name, _client);
+                else
+                    yield return new IpfsFile(link.Id, link.Name, _client);
+            }
+
+        }
+    }
+}
