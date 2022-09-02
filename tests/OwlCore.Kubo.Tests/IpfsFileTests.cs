e472ead4e6efe75f217d7f08ce15c780efbe39e6
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-09-02 13:49:01 -0500

    Added tests for IpfsFile and IpfsFolder

---
+++ tests/OwlCore.Kubo.Tests/IpfsFileTests.cs
@@ -0,0 +1,25 @@
+﻿using Ipfs.Http;
+using OwlCore.Storage;
+using System.IO;
+using System.Threading.Channels;
+
+namespace OwlCore.Kubo.Tests
+{
+    [TestClass]
+    public class IpfsFileTests
+    {
+        [TestMethod]
+        public async Task BasicFileReadTest()
+        {
+            await KuboAccess.TryInitAsync();
+
+            var file = new IpfsFile("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD", KuboAccess.Ipfs);
+            using var stream = await file.OpenStreamAsync();
+
+            using StreamReader text = new StreamReader(stream);
+            var txt = await text.ReadToEndAsync();
+
+            Assert.AreEqual("hello world", txt);
+        }
+    }
+}
---
+++ tests/OwlCore.Kubo.Tests/IpfsFolderTests.cs
@@ -0,0 +1,27 @@
+﻿using Ipfs.Http;
+using OwlCore.Storage;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Channels;
+
+namespace OwlCore.Kubo.Tests
+{
+    [TestClass]
+    public class IpfsFolderTests
+    {
+        [TestMethod]
+        public async Task BasicFolderTraversal()
+        {
+            await KuboAccess.TryInitAsync();
+
+            var folder = new IpfsFolder("QmSnuWmxptJZdLJpKRarxBMS2Ju2oANVrgbr2xWbie9b2D", KuboAccess.Ipfs);
+            var items = await folder.GetItemsAsync().ToListAsync();
+            
+            foreach(var item in items)
+                Debug.WriteLine($"{item.Name}, {item.Id}");
+
+            Assert.IsNotNull(items);
+            Assert.IsTrue(items.Count > 2);
+        }
+    }
+}
