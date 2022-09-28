4316ad86d38bb01d0a468df6e8f71854911bb3bf
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-09-27 19:03:12 -0500

    Updated tests

--- tests/OwlCore.Kubo.Tests/AesPasswordEncryptedPubSubTests.cs
+++ tests/OwlCore.Kubo.Tests/AesPasswordEncryptedPubSubTests.cs
@@ -9,27 +9,8 @@
             await KuboAccess.TryInitAsync();
 
             var encryptedPubsub = new AesPasswordEncryptedPubSub(KuboAccess.Ipfs.PubSub, password: "testing");
-
-            var messageCount = 0;
             
-            var topic = "test";
-            var cs = new CancellationTokenSource();
-            try
-            {
-                await encryptedPubsub.SubscribeAsync(topic, msg =>
-                {
-                    Interlocked.Increment(ref messageCount);
-                }, cs.Token);
-
-                await encryptedPubsub.PublishAsync(topic, "hello world!");
-
-                await Task.Delay(1000);
-                Assert.AreEqual(1, messageCount);
-            }
-            finally
-            {
-                cs.Cancel();
-            }
+            await encryptedPubsub.PublishAsync("owlcore-kubo-test-runner", "hello world!");
         }
     }
 }
