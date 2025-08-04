b2f5edc9d6eb9027ee5e3965d0cac85ddfb445dc
Arlo <arlo.godfrey@outlook.com>
2025-08-04 11:11:45 -0500

    Release 0.22.0. Updated OwlCore.Storage to 0.13.0, implemented ICreateRenamedCopyOf, IMoveRenamedFrom in MfsFolder.

--- src/MfsFolder.Modifiable.cs
+++ src/MfsFolder.Modifiable.cs
@@ -5,7 +5,7 @@ using OwlCore.Storage;
 
 namespace OwlCore.Kubo;
 
-public partial class MfsFolder : IModifiableFolder, IMoveFrom, ICreateCopyOf
+public partial class MfsFolder : IModifiableFolder, ICreateRenamedCopyOf, IMoveRenamedFrom
 {
     /// <summary>
     /// The interval that MFS should be checked for updates.
@@ -27,22 +27,32 @@ public partial class MfsFolder : IModifiableFolder, IMoveFrom, ICreateCopyOf
     }
 
     /// <inheritdoc/>
-    public async Task<IChildFile> CreateCopyOfAsync(IFile fileToCopy, bool overwrite, CancellationToken cancellationToken, CreateCopyOfDelegate fallback)
+    public Task<IChildFile> CreateCopyOfAsync(IFile fileToCopy, bool overwrite, CancellationToken cancellationToken, CreateCopyOfDelegate fallback)
+    {
+        // For code deduplication in this implementation,
+        // route to the overload with rename support
+        // while using the given non-rename overload as fallback.
+        // This also discards the filled newName param in the fallback, which is originally passed into the newName param in the following method call:
+        return CreateCopyOfAsync(fileToCopy, overwrite, newName: fileToCopy.Name, cancellationToken, (modifiableFolder, file, overwrite, _, cancellationToken) => fallback(modifiableFolder, file, overwrite, cancellationToken));
+    }
+
+    /// <inheritdoc/>
+    public async Task<IChildFile> CreateCopyOfAsync(IFile fileToCopy, bool overwrite, string newName, CancellationToken cancellationToken, CreateRenamedCopyOfDelegate fallback)
     {
         if (fileToCopy is MfsFile mfsFile)
-            return await CreateCopyOfAsync(mfsFile, overwrite, cancellationToken);
+            return await CreateCopyOfAsync(mfsFile, overwrite, newName, cancellationToken);
 
         if (fileToCopy is IpfsFile ipfsFile)
-            return await CreateCopyOfAsync(ipfsFile, overwrite, cancellationToken);
+            return await CreateCopyOfAsync(ipfsFile, overwrite, newName, cancellationToken);
 
         if (fileToCopy is IpnsFile ipnsFile)
-            return await CreateCopyOfAsync(ipnsFile, overwrite, cancellationToken);
+            return await CreateCopyOfAsync(ipnsFile, overwrite, newName, cancellationToken);
 
         if (fileToCopy is IGetCid getCid)
         {
             var cid = await getCid.GetCidAsync(cancellationToken);
             
-            var newPath = $"{Path}{fileToCopy.Name}";
+            var newPath = $"{Path}{newName}";
             await Client.Mfs.CopyAsync($"/ipfs/{cid}", newPath, cancel: cancellationToken);
             return new MfsFile(newPath, Client);
         }
@@ -51,49 +61,83 @@ public partial class MfsFolder : IModifiableFolder, IMoveFrom, ICreateCopyOf
         {
             var cid = await addFileToGetCid.GetCidAsync(AddFileOptions, cancellationToken);
             
-            var newPath = $"{Path}{fileToCopy.Name}";
+            var newPath = $"{Path}{newName}";
             await Client.Mfs.CopyAsync($"/ipfs/{cid}", newPath, cancel: cancellationToken);
             return new MfsFile(newPath, Client);
         }
         
-        return await fallback(this, fileToCopy, overwrite, cancellationToken);
+        return await fallback(this, fileToCopy, overwrite, newName, cancellationToken);
     }
 
     /// <inheritdoc/>
     public Task<IChildFile> MoveFromAsync(IChildFile fileToMove, IModifiableFolder source, bool overwrite, CancellationToken cancellationToken, MoveFromDelegate fallback)
     {
-        if (fileToMove is MfsFile mfsFile)
-            return MoveFromAsync(mfsFile, source, overwrite, cancellationToken);
+        // For code deduplication in this implementation,
+        // route to the overload with rename support
+        // while using the given non-rename overload as fallback.
+        // This also discards the filled newName param in the fallback, which is originally passed into the newName param in the following method call:
+        return MoveFromAsync(fileToMove, source, overwrite, newName: fileToMove.Name, cancellationToken, (modifiableFolder, file, source, overwrite, _, cancellationToken) => fallback(modifiableFolder, file, source, overwrite, cancellationToken));
+    }
 
-        return fallback(this, fileToMove, source, overwrite, cancellationToken);
+    /// <inheritdoc/>
+    public Task<IChildFile> MoveFromAsync(IChildFile fileToMove, IModifiableFolder source, bool overwrite, string newName, CancellationToken cancellationToken, MoveRenamedFromDelegate fallback)
+    {
+        if (fileToMove is MfsFile mfsFile)
+            return MoveFromAsync(mfsFile, source, overwrite, newName, cancellationToken);
+
+        return fallback(this, fileToMove, source, overwrite, newName, cancellationToken);
     }
 
     /// <inheritdoc cref="CreateCopyOfAsync(IFile,bool,CancellationToken,CreateCopyOfDelegate)"/>
-    public virtual async Task<IChildFile> CreateCopyOfAsync(MfsFile fileToCopy, bool overwrite = false, CancellationToken cancellationToken = default)
+    public virtual Task<IChildFile> CreateCopyOfAsync(MfsFile fileToCopy, bool overwrite = false, CancellationToken cancellationToken = default)
+    {
+        // For code deduplication in this implementation,
+        // route to the overload with rename support
+        return CreateCopyOfAsync(fileToCopy, overwrite, newName: fileToCopy.Name, cancellationToken);
+    }
+
+    /// <inheritdoc cref="CreateCopyOfAsync(IFile, bool, string, CancellationToken, CreateRenamedCopyOfDelegate)"/>
+    public virtual async Task<IChildFile> CreateCopyOfAsync(MfsFile fileToCopy, bool overwrite, string newName, CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var newPath = $"{Path}{fileToCopy.Name}";
+        var newPath = $"{Path}{newName}";
         await Client.Mfs.CopyAsync(fileToCopy.Path, newPath, cancel: cancellationToken);
         return new MfsFile(newPath, Client);
     }
 
     /// <inheritdoc cref="CreateCopyOfAsync(IFile,bool,CancellationToken,CreateCopyOfDelegate)"/>
-    public virtual async Task<IChildFile> CreateCopyOfAsync(IpfsFile fileToCopy, bool overwrite = false, CancellationToken cancellationToken = default)
+    public virtual Task<IChildFile> CreateCopyOfAsync(IpfsFile fileToCopy, bool overwrite = false, CancellationToken cancellationToken = default)
+    {
+        // For code deduplication in this implementation,
+        // route to the overload with rename support
+        return CreateCopyOfAsync(fileToCopy, overwrite, newName: fileToCopy.Name, cancellationToken);
+    }
+
+    /// <inheritdoc cref="CreateCopyOfAsync(IFile, bool, string, CancellationToken, CreateRenamedCopyOfDelegate)"/>
+    public virtual async Task<IChildFile> CreateCopyOfAsync(IpfsFile fileToCopy, bool overwrite, string newName, CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var newPath = $"{Path}{fileToCopy.Name}";
+        var newPath = $"{Path}{newName}";
         await Client.Mfs.CopyAsync($"/ipfs/{fileToCopy.Id}", newPath, cancel: cancellationToken);
         return new MfsFile(newPath, Client);
     }
 
     /// <inheritdoc cref="CreateCopyOfAsync(IFile,bool,CancellationToken,CreateCopyOfDelegate)"/>
-    public virtual async Task<IChildFile> CreateCopyOfAsync(IpnsFile fileToCopy, bool overwrite = false, CancellationToken cancellationToken = default)
+    public virtual Task<IChildFile> CreateCopyOfAsync(IpnsFile fileToCopy, bool overwrite = false, CancellationToken cancellationToken = default)
+    {
+        // For code deduplication in this implementation,
+        // route to the overload with rename support
+        return CreateCopyOfAsync(fileToCopy, overwrite, newName: fileToCopy.Name, cancellationToken);
+    }
+
+    /// <inheritdoc cref="CreateCopyOfAsync(IFile, bool, string, CancellationToken, CreateRenamedCopyOfDelegate)"/>
+    public virtual async Task<IChildFile> CreateCopyOfAsync(IpnsFile fileToCopy, bool overwrite, string newName, CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var newPath = $"{Path}{fileToCopy.Name}";
+        var newPath = $"{Path}{newName}";
         var cid = await fileToCopy.GetCidAsync(cancellationToken);
         await Client.Mfs.CopyAsync($"/ipfs/{cid}", newPath, cancel: cancellationToken);
 
@@ -101,11 +145,19 @@ public partial class MfsFolder : IModifiableFolder, IMoveFrom, ICreateCopyOf
     }
 
     /// <inheritdoc cref="MoveFromAsync(IChildFile,IModifiableFolder,bool,CancellationToken,MoveFromDelegate)"/>
-    public virtual async Task<IChildFile> MoveFromAsync(MfsFile fileToMove, IModifiableFolder source, bool overwrite = false, CancellationToken cancellationToken = default)
+    public virtual Task<IChildFile> MoveFromAsync(MfsFile fileToMove, IModifiableFolder source, bool overwrite = false, CancellationToken cancellationToken = default)
+    {
+        // For code deduplication in this implementation,
+        // route to the overload with rename support
+        return MoveFromAsync(fileToMove, source, overwrite, newName: fileToMove.Name, cancellationToken);
+    }
+
+    /// <inheritdoc cref="MoveFromAsync(IChildFile, IModifiableFolder, bool, string, CancellationToken, MoveRenamedFromDelegate)"/>
+    public virtual async Task<IChildFile> MoveFromAsync(MfsFile fileToMove, IModifiableFolder source, bool overwrite, string newName, CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var newPath = $"{Path}{fileToMove.Name}";
+        var newPath = $"{Path}{newName}";
         await Client.Mfs.MoveAsync(fileToMove.Path, newPath, cancellationToken);
         return new MfsFile(newPath, Client);
     }
